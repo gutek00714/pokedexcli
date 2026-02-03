eca2eb0b7e68dc086854d2032d723ec72423f318
gutek00714 <guciolaczkowski1@gmail.com>
2026-02-03 01:26:10 +0100

    Add cleanInput + tests

---
+++ repl.go
@@ -0,0 +1,8 @@
+package main
+
+import "strings"
+
+func cleanInput(text string) []string {
+	lowered := strings.ToLower(text)
+	return strings.Fields(lowered)
+}
---
+++ repl_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("%v - lenghts don't match", actual)
+			continue
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("cleanInput(%v) == %v, expected %v", c.input, word, expectedWord)
+			}
+		}
+	}
+}
